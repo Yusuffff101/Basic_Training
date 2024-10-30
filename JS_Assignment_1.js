3d357c2c1ec6d5014c942236aabfb8fa1a10939b
Yusuf <yusufbootwala83@gmail.com>
2024-10-30 16:49:46 +0530

    First Question

--- JS_Assignment_1.js
+++ JS_Assignment_1.js
@@ -1,22 +1,19 @@
-
 function secondLargest(array) {
     if (array.length < 2) {
         return null; 
     }
 
     let first = -Infinity; 
-    let second = -Infinity; 
+    let second = -Infinity;
+
     for (let i = 0; i < array.length; i++) {
         if (array[i] > first) {
+            second = first;
             first = array[i];
-        }
-    }
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > second && array[i] < first) {
+        } else if (array[i] > second && array[i] < first) {
             second = array[i];
         }
     }
 
     return second === -Infinity ? null : second;
 }
-
